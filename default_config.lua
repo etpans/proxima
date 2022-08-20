d3dec34891e9300220365bbebcc7f9ef5a1e61c2
2kbyte <83149837+2kbyte@users.noreply.github.com>
2022-08-19 23:33:45 -0400

    Delete default_config.lua

--- default_config.lua
+++
@@ -1,1 +0,0 @@
-return '{"Menu Background Color":[0,0,0],"UI Background":"Floral","UI Toggle":"BackSlash","Menu Background Color Transparency":1,"Menu Accent Color":[0.7411764860153198,0.5764706134796143,0.9764705896377564],"Tile Size":220}'
